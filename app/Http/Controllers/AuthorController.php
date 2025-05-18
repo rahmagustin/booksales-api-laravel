e154a3dd4579eca7e2d6ad3af095f8d99ec52aad
rahmagustin <e41220949@student.polije.ac.id>
2025-05-18 22:03:16 +0700

    tugas laravel 3

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -10,6 +10,10 @@ class AuthorController extends Controller
     public function index() {
         $authors = Author::all();
 
-        return view('authors', ['authors' => $authors]);
+        return response()->json([
+            "success" => true,
+            "message" => "Get All Resource",
+            "data" => $authors
+        ], 200);
     }
 }
--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -10,6 +10,10 @@ class BookController extends Controller
     public function index() {
         $books = Book::all();
 
-        return view('books', ['books' => $books]);
+         return response()->json([
+            "success" => true,
+            "message" => "Get All Resource",
+            "data" => $books
+        ], 200);
     }
 }
